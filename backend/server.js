8eb784eb0e57537914826447e9a4587d23499b8e
intehon <emelie.soderhielm@gmail.com>
2022-01-17 20:23:40 +0100

    update backend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/final-project"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
