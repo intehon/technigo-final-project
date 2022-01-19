0af05bbe5cfe092a7da4be8656305e5b8ccf9b72
intehon <emelie.soderhielm@gmail.com>
2022-01-19 11:23:17 +0100

    fix backend issue

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import mongoose from 'mongoose'
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+import listEndpoints from 'express-list-endpoints'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -19,11 +15,12 @@ app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send(listEndpoints(app))
 })
+
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
+  console.log(`Server running on http://localhost:${port} ~'(@)~'`)
 })
