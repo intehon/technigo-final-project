153acbdbac61baad3698a5cdc773fa8945d7212b
intehon <emelie.soderhielm@gmail.com>
2022-04-16 23:52:34 +0200

    try another import of cloudinary

--- backend/routes/menu.js
+++ backend/routes/menu.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import auth from '../middleware/auth.js'
-// import cloudinaryFramwork from 'cloudinary'
+import cloudinaryFramwork from 'cloudinary'
 import { v2 as cloudinary } from 'cloudinary'
 import multer from 'multer'
 import { CloudinaryStorage } from 'multer-storage-cloudinary'
